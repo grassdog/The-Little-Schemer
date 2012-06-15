5dcf6ceea53ce4a3d7248c2fa5b9dbca20e96b8e
Ray Grasso <ray.grasso@gmail.com>
2012-06-15 20:46:39 +0800

    Finished chapter 5

--- chapter05.rkt
+++ chapter05.rkt
@@ -56,3 +56,72 @@
                   (subst* (cdr l)))))))
 
 
+; Insert left in a S-expression
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+        (cond
+          ((eq? (car l) old) (cons new
+                                   (cons old
+                                         (insertL* new old (cdr l)))))
+          (else (cons (car l)
+                      (insertL* new old (cdr l))))))
+      (else (cons (insertL* new old (car l))
+                  (insertL* new old (cdr l)))))))
+
+; Member in a S-expression
+(define member*
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((atom? (car l))
+        (or (eq? (car l) a)
+            (member* a (cdr l))))
+      (else (or (member* a (car l))
+                (member* a (cdr l)))))))
+
+; Leftmost atom in a S-expression that doesn't contain empty lists
+(define leftmost
+  (lambda (l)
+    (cond
+      ((atom? (car l)) (car l))
+      (else (leftmost (car l))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (= a1 a2))
+      ((or (number? a1) (number? a2)) #f)
+      (else (eq? a1 a2)))))
+
+; List equality (using eqan)
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      ((and (atom? (car l1)) (atom? (car l2))) (and (eqan? (car l1) (car l2))
+                                                    (eqlist? (cdr l1) (cdr l2))))
+      ((or (atom? (car l1)) (atom? (car l2))) #f)
+      (else (and (eqlist? (car l1) (car l2))
+                 (eqlist? (cdr l2) (cdr l2)))))))
+
+(define myequal?
+  (lambda (s1 s2)
+    (cond
+      ((and (atom? s1) (atom? s2)) (eqan? s1 s2))
+      ((or (atom? s1) (atom? s2)) #f)
+      (else (eqlist? s1 s2)))))
+
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      (else (and (myequal? (car l1) (car l2))
+                 (eqlist? (cdr l2) (cdr l2)))))))
+
+
